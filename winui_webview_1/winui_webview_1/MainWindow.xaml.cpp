2822a16873af5fcfd3eb1dfadbd9e964df3b38e9
lilac <github@nijisanji.sakura.ne.jp>
2021-06-15 00:10:42 +0900

    プロジェクト ファイルを追加します。

---
+++ winui_webview_1/winui_webview_1/MainWindow.xaml.cpp
@@ -0,0 +1,51 @@
+﻿#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::winui_webview_1::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+    }
+}
+
+void winrt::winui_webview_1::implementation::MainWindow::get_button_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+{
+    winrt::hstring sss = url_text().Text();
+
+    
+    try 
+    {
+        if (sss.size() == 0)
+        {
+            MessageBox(NULL, L"urlに何もないよ", L"urlに何もないよ", MB_OK);
+            return;
+        }
+
+        winrt::Windows::Foundation::Uri uri(url_text().Text());
+        MyWebView().Source(uri);
+    }
+    catch (winrt::hresult_error const& e)
+    {
+        winrt::hstring err = e.message();
+        
+        MessageBox(NULL, err.c_str(),L"えらー", MB_OK);
+    }
+    
+
+}
+
+
+void winrt::winui_webview_1::implementation::MainWindow::MenuFlyoutItem_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+{
+    url_text().PasteFromClipboard();
+}
---
+++ winui_webview_1/winui_webview_1/MainWindow.xaml.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#pragma push_macro("GetCurrentTime")
+#undef GetCurrentTime
+
+#include "MainWindow.g.h"
+
+#pragma pop_macro("GetCurrentTime")
+
+namespace winrt::winui_webview_1::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+        void get_button_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+        void MenuFlyoutItem_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+    };
+}
+
+namespace winrt::winui_webview_1::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
